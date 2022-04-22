889c31f8b78a1ef117df731b690fd6994167b1ff
ramzan1798 <mr450856@gmail.com>
2022-04-22 12:06:55 +0500

    updated

--- src/calculator.js
+++ src/calculator.js
@@ -16,6 +16,7 @@ const reRenderEditor = () => {
 const reRenderOutput = () => {
     viewer.innerText = (() => {
         let wordListQueue = [];
+
         editorQueue.forEach(elm => {
             if (wordListQueue.length === 0) {
                 wordListQueue.push(elm);
@@ -38,13 +39,56 @@ const reRenderOutput = () => {
                 }
             }
         });
-        console.log(wordListQueue);
-        return '';
+
+        if (wordListQueue.length <= 2) {
+            return '';
+        }
+
+        for (let i = 0; i < wordListQueue.length; i++) {
+            if (wordListQueue[i] === '%') {
+                wordListQueue[i - 1] = JSON.stringify(parseFloat(wordListQueue[i - 1]) / 100)
+                wordListQueue[i] = '*'
+            }
+        }
+
+        const calcRes = (op, index) => {
+            const operandBefore = parseFloat(wordListQueue[index - 1]);
+            const operandAfter = parseFloat(wordListQueue[index + 1]);
+
+            if (op === '/') {
+                return JSON.stringify(operandBefore / operandAfter);
+            } else if (op === '*') {
+                return JSON.stringify(operandBefore * operandAfter);
+            } else if (op === '+') {
+                return JSON.stringify(operandBefore + operandAfter);
+            } else {
+                return JSON.stringify(operandBefore - operandAfter);
+            }
+        }
+
+        for (let stage = 1; stage <= 4; stage++) {
+            for (let i = 0; i < wordListQueue.length; ) {
+                const sliceBefore = wordListQueue.slice(0, i - 1);
+                const sliceAfter = wordListQueue.slice(i + 2, wordListQueue.length);
+                if (stage === 1 && wordListQueue[i] === '/' && i !== wordListQueue.length - 1) {
+                    wordListQueue = [...sliceBefore, calcRes(wordListQueue[i], i), ...sliceAfter];
+                } else if (stage === 2 && wordListQueue[i] === '*' && i !== wordListQueue.length - 1) {
+                    wordListQueue = [...sliceBefore, calcRes(wordListQueue[i], i), ...sliceAfter];
+                } else if (stage === 3 && wordListQueue[i] === '+' && i !== wordListQueue.length - 1) {
+                    wordListQueue = [...sliceBefore, calcRes(wordListQueue[i], i), ...sliceAfter];
+                    console.log(editorQueue);
+                    console.log(wordListQueue);
+                } else if (stage === 4 && wordListQueue[i] === '-' && i !== wordListQueue.length - 1) {
+                    wordListQueue = [...sliceBefore, calcRes(wordListQueue[i], i), ...sliceAfter];
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        return wordListQueue.length === 0 ? '' : parseFloat(wordListQueue[0]).toFixed(2)
     })();
 }
-
-editorQueue = ['1', '2', '3', '-', '2', '4', '5', '*', '6', '/', '8', '9', '+', '4', '%', '4'];
-reRenderOutput();
 
 const numericKeys = [
     document.querySelector('.zero'),
@@ -63,7 +107,8 @@ numericKeys.forEach(numericKey => {
     numericKey.addEventListener('click', e => {
         editorQueue.push(e.target.innerText);
         reRenderEditor();
-    })
+        reRenderOutput();
+    });
 });
 
 document.querySelector('.clr').addEventListener('click', e => {
@@ -73,17 +118,46 @@ document.querySelector('.clr').addEventListener('click', e => {
 });
 
 document.querySelector('.del').addEventListener('click', e => {
-    editorQueue.pop()
+    editorQueue.pop();
     reRenderEditor();
-    viewer.innerText = 'TODO: //'  // TODO:
+    reRenderOutput();
 });
 
-const add = document.querySelector('.add');
-const sub = document.querySelector('.sub');
-const mul = document.querySelector('.mul');
+[
+    document.querySelector('.add'),
+    document.querySelector('.sub'),
+    document.querySelector('.mul'),
+    document.querySelector('.divide'),
+    document.querySelector('.percentage')
+].forEach(operator => {
+    operator.addEventListener('click', e => {
+        const lastElement = editorQueue.pop();
 
-const point = document.querySelector('.point');
+        if (!['-', '+', '*', '/', '%', '.'].includes(lastElement) && lastElement !== undefined) {
+            editorQueue.push(lastElement);
+            editorQueue.push(e.target.innerText);
+            reRenderEditor();
+            reRenderOutput();
+        }
+    });
+});
 
-const percentage = document.querySelector('.percentage');
+document.querySelector('.point').addEventListener('click', e => {
+    let lastOperatorIndex = -1;
+    for (let i = 0; i < editorQueue.length; i++) {
+        if (['+', '-', '*', '/', '%'].includes(editorQueue[i])) {
+            lastOperatorIndex = i;
+        }
+    }
+    const sliceAfterLastOperator = editorQueue.slice(lastOperatorIndex + 1, editorQueue.length);
+    if (!sliceAfterLastOperator.includes('.')) {
+        const lastElement = editorQueue.pop();
+        editorQueue.push(lastElement);
 
-const divide = document.querySelector('.divide');
+        if (lastElement !== undefined) {
+            editorQueue.push('.');
+            reRenderEditor();
+            reRenderOutput();
+        }
+    }
+});
